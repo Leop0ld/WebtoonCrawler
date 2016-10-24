8ff486235dac7ea6ebac3f539a0b8c3f84575038
Leop0ld <l3opold7@gmail.com>
2016-10-24 23:01:38 +0900

    Today webtoon crawling

--- crawler.py
+++ crawler.py
@@ -1,15 +1,43 @@
 from bs4 import BeautifulSoup
 import requests
-import lxml
+import re
+from lxml import html
 
 
 BASE_URL = 'http://comic.naver.com/webtoon/weekday.nhn'
 
 
 def get_html(url, headers=None):
+    """
+    :param url: get 요청을 보낼 url
+    :param headers: get 요청에 사용될 header
+    :return html: BeautifulSoup으로 get 요청에 대한 html 값을 파싱해서 반환한다
+    """
     response = requests.get(url, headers=headers)
-    html_content = response.text.encode(response.encoding)
-    html = BeautifulSoup(html_content, "lxml")
+    decode_text = response.text.encode(response.encoding)
+    html = BeautifulSoup(decode_text, "lxml")
     return html
 
-print(get_html(BASE_URL))
+
+def search_weekday(html_str):
+    """
+    :param html_str: bs4로 파싱된 html의 string을 받는다
+    :return result: 무슨 요일 웹툰인지 확인하기 위해 요일을 반환한다(e.g. 월요 웹툰)
+    """
+    tree = html.fromstring(html_str)
+    result = tree.xpath('//*[@id="content"]/div[3]/div[1]/div/h4/span/text()')[0]
+    return result
+
+
+def search_today_webtoon_list(html_str):
+    """
+    :param html_str: bs4로 파싱된 html의 string을 받는다
+    :return result: 오늘의 웹툰 리스트를 result에 담아서 list 형태로 반환한다
+    """
+    tree = html.fromstring(html_str)
+    result = tree.xpath('//*[@id="content"]/div[3]/div[1]/div/ul/li/a/text()')
+    return result
+
+html_content = str(get_html(BASE_URL))
+today_webtoon_list = search_today_webtoon_list(html_content)
+weekday = search_weekday(html_content)
--- tests.py
+++ tests.py
@@ -1,7 +1,24 @@
-from crawler import get_html
+from crawler import get_html, BASE_URL, search_today_webtoon_list, search_weekday
+
+import time
 
 
 def test_correct_get_html():
-    html = get_html('http://comic.naver.com/webtoon/weekday.nhn')
+    html = get_html(BASE_URL)
     assert str(html).startswith('<!DOCTYPE html>')
     assert str(html).endswith('</html>\n')
+
+
+def test_correct_return_today_webtoon():
+    html = get_html(BASE_URL)
+    today_webtoon_list = search_today_webtoon_list(str(html))
+    assert len(today_webtoon_list)
+
+
+def test_correct_weekday():
+    today = time.localtime()
+    week = ('월', '화', '수', '목', '금', '토', '일')
+
+    html = get_html(BASE_URL)
+    weekday = search_weekday(str(html))
+    assert weekday == week[today.tm_wday] + '요 웹툰'
