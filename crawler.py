46074c20fa9a1a0af9264efcee9ae68b0acafaa8
Leop0ld <l3opold7@gmail.com>
2016-10-30 11:05:52 +0900

    coverage 파일 제거 및 download_image 함수 추가

--- crawler.py
+++ crawler.py
@@ -1,10 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import sys
+import time
 from lxml import html
 
+BASE_URL = 'http://comic.naver.com'
+WEBTOON_LIST_URL = 'http://comic.naver.com/webtoon/weekday.nhn'
+DETAIL_WEBTOON_URL = 'http://comic.naver.com/webtoon/list.nhn?titleId='
 
-BASE_URL = 'http://comic.naver.com/webtoon/weekday.nhn'
+today = time.localtime()
+
+header = {
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
+    'Accept': 'image/webp,image/*,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    'Accept-Language': 'ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4,zh-CN;q=0.2,zh;q=0.2',
+}
 
 
 def get_html(url, headers=None):
@@ -16,7 +29,7 @@ def get_html(url, headers=None):
     response = requests.get(url, headers=headers)
     decode_text = response.text.encode(response.encoding)
     html = BeautifulSoup(decode_text, "lxml")
-    return html
+    return str(html)
 
 
 def search_weekday(html_str):
@@ -25,7 +38,7 @@ def search_weekday(html_str):
     :return result: 무슨 요일 웹툰인지 확인하기 위해 요일을 반환한다(e.g. 월요 웹툰)
     """
     tree = html.fromstring(html_str)
-    result = tree.xpath('//*[@id="content"]/div[3]/div[1]/div/h4/span/text()')[0]
+    result = tree.xpath('//*[@id="content"]/div[3]/div[@class="col col_selected"]/div/h4/span/text()')[0]
     return result
 
 
@@ -35,9 +48,62 @@ def search_today_webtoon_list(html_str):
     :return result: 오늘의 웹툰 리스트를 result에 담아서 list 형태로 반환한다
     """
     tree = html.fromstring(html_str)
-    result = tree.xpath('//*[@id="content"]/div[3]/div[1]/div/ul/li/a/text()')
+    result = tree.xpath('//*[@id="content"]/div[3]/div[@class="col col_selected"]/div/ul/li/a')
     return result
 
-html_content = str(get_html(BASE_URL))
+
+def download_webtoon(webtoon_id):
+    """
+    :param webtoon_id: webtoon의 id를 받는다
+    :return 은 하지 않고 webtoon의 최신 날짜의 것을 다운로드한다
+    """
+    url = DETAIL_WEBTOON_URL + webtoon_id
+    response = get_html(url)
+    tree = html.fromstring(response)
+    a_list = tree.xpath('//*[@id="content"]/table/tr')
+    detail_url = a_list[2][1][0].attrib['href']
+
+    detail_webtoon_html = get_html(BASE_URL+detail_url)
+    image_tree = html.fromstring(detail_webtoon_html)
+
+    # Webtoon Image list
+    image_list = image_tree.xpath('//*[@alt="comic content"]')
+    index = 1
+    for image in image_list:
+        #download_image(image.attrib['src'], str(index)+'.jpg', 'webtoon/')
+        index += 1
+
+    pass
+
+
+def download_image(img_url, file_name, file_path=''):
+    """
+    :param img_url: img를 가져올 url을 지정한다
+    :param file_name: file을 어떤 이름으로 저장할 것인지 지정한다
+    :param file_path: default 값은 python 파일이 위치한 곳이지만 다른 곳에 다운받고 싶을 경우 지정하면 된다
+    :return 하지 않고 요청한 url에 있는 image를 다운받는다
+    """
+    with open(file_path+file_name, "wb") as f:
+        print("Downloading %s" % file_name)
+        response = requests.get(img_url, headers=header, stream=True)
+        total_length = response.headers.get('content-length')
+
+        if total_length is None:  # no content length header
+            f.write(response.content)
+        else:
+            dl = 0
+            total_length = int(total_length)
+            for data in response.iter_content(chunk_size=4096):
+                dl += len(data)
+                f.write(data)
+                done = int(50 * dl / total_length)
+                sys.stdout.write("[%s%s]\n" % ('=' * done, ' ' * (50 - done)))
+                time.sleep(0.5)
+                sys.stdout.flush()
+
+
+html_content = get_html(WEBTOON_LIST_URL)
 today_webtoon_list = search_today_webtoon_list(html_content)
 weekday = search_weekday(html_content)
+
+download_webtoon('675823')
--- tests.py
+++ tests.py
@@ -1,24 +1,34 @@
-from crawler import get_html, BASE_URL, search_today_webtoon_list, search_weekday
+from crawler import get_html, search_today_webtoon_list, search_weekday, today as crawl_day, download_webtoon, \
+                    BASE_URL, WEBTOON_LIST_URL
 
 import time
 
 
+def test_today_is_correct():
+    assert crawl_day.tm_wday == time.localtime().tm_wday
+
+
 def test_correct_get_html():
-    html = get_html(BASE_URL)
-    assert str(html).startswith('<!DOCTYPE html>')
-    assert str(html).endswith('</html>\n')
+    html = get_html(WEBTOON_LIST_URL)
+    assert html.startswith('<!DOCTYPE html>')
+    assert html.endswith('</html>\n')
 
 
 def test_correct_return_today_webtoon():
-    html = get_html(BASE_URL)
-    today_webtoon_list = search_today_webtoon_list(str(html))
+    html = get_html(WEBTOON_LIST_URL)
+    today_webtoon_list = search_today_webtoon_list(html)
     assert len(today_webtoon_list)
 
 
 def test_correct_weekday():
-    today = time.localtime()
     week = ('월', '화', '수', '목', '금', '토', '일')
+    html = get_html(WEBTOON_LIST_URL)
+    weekday = search_weekday(html)
+    assert weekday == week[crawl_day.tm_wday] + '요 웹툰'
 
-    html = get_html(BASE_URL)
-    weekday = search_weekday(str(html))
-    assert weekday == week[today.tm_wday] + '요 웹툰'
+
+def test_download_webtoon():
+    webtoon_id = '25455'
+    download_webtoon(webtoon_id)
+    with open('webtoon/1.jpg', 'rb') as f:
+        assert f.name
